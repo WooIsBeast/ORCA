9176ebef83570f13cd443142d401b0d755ffd51e
WooIsBeast <88990499+WooIsBeast@users.noreply.github.com>
2021-12-09 12:32:44 +0800

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,28 @@
+ScrollReveal().reveal('.home-img', {
+    delay: 1500,
+    duration: 1000,
+    origin:'bottom',
+    distance: '400px',
+    reset: true
+});
+ScrollReveal().reveal('.home-title', {
+    delay: 1500,
+    duration: 1000,
+    origin:'right',
+    distance: '400px',
+    reset: true
+});
+ScrollReveal().reveal('.about-title', {
+    delay: 1500,
+    duration: 1000,
+    origin:'top',
+    distance: '400px',
+    reset: true
+});
+ScrollReveal().reveal('.games-section', {
+    delay: 1500,
+    duration: 1000,
+    origin:'top',
+    distance: '400px',
+    reset: true
+});
